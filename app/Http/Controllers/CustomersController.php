07e2f87f213639e64107428cb58d2704ef9514cc
K. M. Zawad Monsur <zawadmonsur1@gmail.com>
2022-08-26 17:40:34 +0600

    Practice upto EP-13

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -9,8 +9,8 @@ class CustomersController extends Controller
 {
     public function list()
     {
-        $activeCustomers = Customer::where('active', 1)->get();
-        $inactiveCustomers = Customer::where('active', 0)->get();
+        $activeCustomers = Customer::active()->get();
+        $inactiveCustomers = Customer::inactive()->get();
 
 
         return view('internals.customers', compact('activeCustomers', 'inactiveCustomers'));
@@ -26,15 +26,7 @@ class CustomersController extends Controller
             'active' => 'required'
         ]);
 
-
-        $customer = new Customer();
-        $customer->name = request('name');
-        $customer->email = request('email');
-        $customer->number = request('number');
-        $customer->active = request('active');
-        $customer->save();
-
-
+        Customer::create($data);
 
         return back();
     }
--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -7,5 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
+    //Fillable Example
+    // protected $fillable = ['name', 'email', 'number', 'active'];
+
+    //Guarded Example
+    protected $guarded = [];
+
     use HasFactory;
+
+    public function scopeActive($query)
+    {
+        return $query->where('active', 1);
+    }
+
+    public function scopeInactive($query)
+    {
+        return $query->where('active', 0);
+    }
 }
